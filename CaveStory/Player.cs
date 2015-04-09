58a523f7197116e8c3ad3d065b4995d9ef3e8f48
Sanders Lauture <golf1052@live.com>
2015-04-08 22:12:17 -0400

    Episode 8: Facings!
    And Properties! I don't even know what is going on. Such a different
    coding style.

--- CaveStory/Player.cs
+++ CaveStory/Player.cs
@@ -14,24 +14,51 @@ namespace CaveStory
         const float WalkingAcceleration = 0.0012f; // (pixels / millisecond) / millisecond
         const float MaxSpeedX = 0.325f; // pixels / millisecond
 
-        Sprite sprite;
         int x;
         int y;
         float velocityX;
         float accelerationX;
+        SpriteState.HorizontalFacing horizontalFacing;
+
+        Dictionary<SpriteState, Sprite> sprites;
+
+        private SpriteState spriteState;
+        public SpriteState SpriteState
+        {
+            get
+            {
+                return new SpriteState(accelerationX == 0.0f ? SpriteState.MotionType.Standing : SpriteState.MotionType.Walking,
+                    horizontalFacing);
+            }
+        }
 
         public Player(ContentManager Content, int x, int y)
         {
-            sprite = new AnimatedSprite(Content, "MyChar", 0, 0, Game1.TileSize, Game1.TileSize, 15, 3);
+            sprites = new Dictionary<SpriteState, Sprite>();
+            InitializeSprites(Content);
             this.x = x;
             this.y = y;
             velocityX = 0;
             accelerationX = 0;
+            horizontalFacing = SpriteState.HorizontalFacing.Left;
+        }
+
+        public void InitializeSprites(ContentManager Content)
+        {
+            sprites.Add(new SpriteState(SpriteState.MotionType.Standing, SpriteState.HorizontalFacing.Left),
+                new Sprite(Content, "MyChar", 0, 0, Game1.TileSize, Game1.TileSize));
+            sprites.Add(new SpriteState(SpriteState.MotionType.Walking, SpriteState.HorizontalFacing.Left),
+                new AnimatedSprite(Content, "MyChar", 0, 0, Game1.TileSize, Game1.TileSize, 15, 3));
+
+            sprites.Add(new SpriteState(SpriteState.MotionType.Standing, SpriteState.HorizontalFacing.Right),
+                new Sprite(Content, "MyChar", 0, Game1.TileSize, Game1.TileSize, Game1.TileSize));
+            sprites.Add(new SpriteState(SpriteState.MotionType.Walking, SpriteState.HorizontalFacing.Right),
+                new AnimatedSprite(Content, "MyChar", 0, Game1.TileSize, Game1.TileSize, Game1.TileSize, 15, 3));
         }
 
         public void Update(GameTime gameTime)
         {
-            sprite.Update(gameTime);
+            sprites[SpriteState].Update(gameTime);
             x += (int)Math.Round(velocityX * gameTime.ElapsedGameTime.TotalMilliseconds);
             velocityX += accelerationX * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
             if (accelerationX < 0.0f)
@@ -51,11 +78,13 @@ namespace CaveStory
         public void StartMovingLeft()
         {
             accelerationX = -WalkingAcceleration;
+            horizontalFacing = SpriteState.HorizontalFacing.Left;
         }
 
         public void StartMovingRight()
         {
             accelerationX = WalkingAcceleration;
+            horizontalFacing = SpriteState.HorizontalFacing.Right;
         }
 
         public void StopMoving()
@@ -65,7 +94,7 @@ namespace CaveStory
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            sprite.Draw(spriteBatch, x, y);
+            sprites[SpriteState].Draw(spriteBatch, x, y);
         }
     }
 }
---
+++ CaveStory/SpriteState.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaveStory
+{
+    public struct SpriteState
+    {
+        public enum MotionType
+        {
+            Standing,
+            Walking
+        }
+
+        public enum HorizontalFacing
+        {
+            Left,
+            Right
+        }
+
+        MotionType motionType;
+        HorizontalFacing horizontalFacing;
+
+        public SpriteState(MotionType motionType = MotionType.Standing,
+            HorizontalFacing horizontalFacing = HorizontalFacing.Left)
+        {
+            this.motionType = motionType;
+            this.horizontalFacing = horizontalFacing;
+        }
+
+        public static bool operator < (SpriteState a, SpriteState b)
+        {
+            if (a.motionType != b.motionType)
+            {
+                return a.motionType < b.motionType;
+            }
+            if (a.horizontalFacing != b.horizontalFacing)
+            {
+                return a.horizontalFacing < b.horizontalFacing;
+            }
+            return false;
+        }
+
+        public static bool operator > (SpriteState a, SpriteState b)
+        {
+            return b < a;
+        }
+    }
+}
