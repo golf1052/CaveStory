610c36ae31ca8893106d38a154a3753b0a9338f1
Sanders Lauture <golf1052@live.com>
2015-05-02 14:57:48 -0400

    Episode 34: Placing polar star projectiles

--- CaveStory/PolarStar.cs
+++ CaveStory/PolarStar.cs
@@ -33,7 +33,27 @@ namespace CaveStory
         static TileUnit LeftOffset { get { return 0; } }
         static TileUnit RightOffset { get { return 1; } }
 
+        // Gun offsets
+        static GameUnit NozzleHorizontalY { get { return 23; } }
+        static GameUnit NozzleHorizontalLeftX { get { return 10; } }
+        static GameUnit NozzleHorizontalRightX { get { return 38; } }
+
+        static GameUnit NozzleUpY { get { return 4; } }
+        static GameUnit NozzleUpLeftX { get { return 27; } }
+        static GameUnit NozzleUpRightX { get { return 21; } }
+
+        static GameUnit NozzleDownY { get { return 28; } }
+        static GameUnit NozzleDownLeftX { get { return 29; } }
+        static GameUnit NozzleDownRightX { get { return 19; } }
+
+        static TileUnit ProjectileSourceY { get { return 2; } }
+        static TileUnit HorizontalProjectileSourceX { get { return 8; } }
+        static TileUnit VerticalProjectileSourceX { get { return 9; } }
+
         Dictionary<PolarStarSpriteState, Sprite> sprites;
+
+        Sprite horizontalProjectile;
+        Sprite verticalProjectile;
 
         public PolarStar(ContentManager Content)
         {
@@ -43,6 +63,12 @@ namespace CaveStory
 
         public void InitializeSprites(ContentManager Content)
         {
+            horizontalProjectile = new Sprite(Content, "Bullet",
+                Units.TileToPixel(HorizontalProjectileSourceX), Units.TileToPixel(ProjectileSourceY),
+                Units.TileToPixel(1), Units.TileToPixel(1));
+            verticalProjectile = new Sprite(Content, "Bullet",
+                Units.TileToPixel(VerticalProjectileSourceX), Units.TileToPixel(ProjectileSourceY),
+                Units.TileToPixel(1), Units.TileToPixel(1));
             for (SpriteState.HorizontalFacing horizontalFacing = SpriteState.HorizontalFacing.FirstHorizontalFacing;
                     horizontalFacing < SpriteState.HorizontalFacing.LastHorizontalFacing;
                     ++horizontalFacing)
@@ -104,6 +130,56 @@ namespace CaveStory
             }
             sprites[new PolarStarSpriteState(new Tuple<SpriteState.HorizontalFacing, SpriteState.VerticalFacing>(
                 horizontalFacing, verticalFacing))].Draw(spriteBatch, x, y);
+
+            GameUnit bulletX = x - Units.HalfTile;
+            GameUnit bulletY = y - Units.HalfTile;
+            switch (verticalFacing)
+            {
+                case SpriteState.VerticalFacing.Horizontal:
+                    bulletY += NozzleHorizontalY;
+                    if (horizontalFacing == SpriteState.HorizontalFacing.Left)
+                    {
+                        bulletX += NozzleHorizontalLeftX;
+                    }
+                    else
+                    {
+                        bulletX += NozzleHorizontalRightX;
+                    }
+                    break;
+                case SpriteState.VerticalFacing.Up:
+                    bulletY += NozzleUpY;
+                    if (horizontalFacing == SpriteState.HorizontalFacing.Left)
+                    {
+                        bulletX += NozzleUpLeftX;
+                    }
+                    else
+                    {
+                        bulletX += NozzleUpRightX;
+                    }
+                    break;
+                case SpriteState.VerticalFacing.Down:
+                    bulletY += NozzleDownY;
+                    if (horizontalFacing == SpriteState.HorizontalFacing.Left)
+                    {
+                        bulletX += NozzleDownLeftX;
+                    }
+                    else
+                    {
+                        bulletX += NozzleDownRightX;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            if (verticalFacing == SpriteState.VerticalFacing.Horizontal)
+            {
+                horizontalProjectile.Draw(spriteBatch, bulletX, bulletY);
+            }
+            else
+            {
+                verticalProjectile.Draw(spriteBatch, bulletX, bulletY);
+            }
         }
     }
 }
